91f04856029eac5eb90b7e782b38662c4c5a6e36
behnia shayeste <92170181+Behniash@users.noreply.github.com>
2025-07-07 01:26:24 -0700

    main.py

--- main.py
+++ main.py
@@ -24,15 +24,12 @@ from src.utils import load_courses, load_professors, load_classrooms
 from src.genetic_algorithm import generate_initial_population
 
 if __name__ == "__main__":
-    # لود داده‌ها
     courses = load_courses(r"D:\University Course Timetabling Optimization\Data\courses.json")
     professors = load_professors(r"D:\University Course Timetabling Optimization\Data\professors.json")
     classrooms = load_classrooms(r"D:\University Course Timetabling Optimization\Data\classes.json")
 
-    # تولید جمعیت اولیه
     population = generate_initial_population(5, courses, professors, classrooms)
 
-    # چاپ نمونه
     print(" Initial Population:")
     for i, chromosome in enumerate(population, 1):
         print(f"\nChromosome {i}:")
@@ -65,7 +62,6 @@ if __name__ == "__main__":
     professors = load_professors(r"D:\University Course Timetabling Optimization\Data\professors.json")
     classrooms = load_classrooms(r"D:\University Course Timetabling Optimization\Data\classes.json")
 
-    # اجرای الگوریتم ژنتیک
     best_timetable, best_fitness = genetic_algorithm(
         courses, professors, classrooms,
         population_size=10, generations=20, mutation_rate=0.2
@@ -87,15 +83,13 @@ if __name__ == "__main__":
     professors = load_professors(r"D:\University Course Timetabling Optimization\Data\professors.json")
     classrooms = load_classrooms(r"D:\University Course Timetabling Optimization\Data\classes.json")
 
-    # اجرای الگوریتم ژنتیک
     best_timetable, best_fitness = genetic_algorithm(
         courses, professors, classrooms,
         population_size=10, generations=20, mutation_rate=0.2
     )
 
-    print("\n🎉 Best Timetable Found (Fitness:", best_fitness, ")")
+    print("\n Best Timetable Found (Fitness:", best_fitness, ")")
     for gene in best_timetable:
         print(gene)
 
-    # رسم نمودار
     plot_timetable(best_timetable, title=f"Best Timetable (Fitness: {best_fitness})")
